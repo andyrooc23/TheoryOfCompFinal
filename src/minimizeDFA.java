c50240665b6cb7cd330b78a6b4a425aa74357542
andrewcantrell <andyroocantrell@gmail.com>
2021-12-07 23:38:12 -0800

    Done with the final project

--- src/minimizeDFA.java
+++ src/minimizeDFA.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-import javafx.util.Pair;
 
 
 public class minimizeDFA {
@@ -72,13 +71,87 @@ public class minimizeDFA {
         }
         //Section 5
 
-        Map<Object, Object> toDelete = new HashMap<>();
+        Map<Integer, Set<Integer>> toDelete = new HashMap<>();
         for(Pair p : grid){
             Pair temp = new Pair(p.x, p.y);
             String first = temp.x;
             String second = temp.y;
             int i = Integer.parseInt(second);
             int j = Integer.parseInt(first);
+
+
+            if(!toDelete.containsKey(j) || toDelete.containsKey(i)){
+                if(!toDelete.containsKey(j) && toDelete.containsKey(i)){
+                    toDelete.put(i, new HashSet<>());
+                    toDelete.get(i).add(j);
+                }else if(toDelete.containsKey(j)){
+                    for(int a : toDelete.keySet()) {
+                        toDelete.get(a).add(i);
+                    }
+                }else if(toDelete.containsKey(i)){
+                    for(int a : toDelete.keySet()) {
+                        toDelete.get(a).add(j);
+                    }
+                }
+            }
+            if(toDelete.containsKey(i) && toDelete.containsKey(j)) {
+                for (int x : toDelete.get(j)) {
+                    toDelete.get(i).add(j);
+                }
+                toDelete.remove(j);
+            }else if(toDelete.containsKey(i)){
+                toDelete.get(i).add(j);
+            }else{
+                toDelete.get(j).add(i);
+            }
+        }
+
+        // Section 6
+        Set<Integer> ds = new HashSet<>();
+        for(int x : toDelete.keySet()){
+            for(int y : toDelete.get(x)){
+                ds.add(y);
+            }
+        }
+        List<Integer> deleteStates = new ArrayList<Integer>(ds);
+        deleteStates.sort();
+        for (int i : deleteStates){
+            for (int edge : edges){
+                int stc = states.get(i).children.get(edge);
+                if(deleteStates.contains(stc)){
+                    for(int x : toDelete.keySet()){
+                        if(toDelete.get(x).contains(i)){
+                            if (i < states.size() && x < states.size()){
+                                states.get(i).children.put(edge, states.get(x).val);
+                            }
+                        }
+                    }
+                }
+            }
+            states.remove(i);
+        }
+
+        // Section 7
+        System.out.print("             ");
+        for(int x : edges){
+            System.out.print(x + "   ");
+        }
+        System.out.println();
+        for(StateNode x : states){
+            System.out.print("state: " + x.val);
+            System.out.print("  | ");
+            for(int y : x.children.values()){
+                System.out.print(y + "   ");
+            }
+            System.out.println();
+        }
+        System.out.println("where: ");
+        for(int x : toDelete.keySet()){
+            System.out.println(x + " maps to ");
+            for (int y : toDelete.get(x)){
+                System.out.print(y + " : ");
+            }
+            System.out.println();
         }
     }
 
